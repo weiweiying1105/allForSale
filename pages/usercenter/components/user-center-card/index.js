412522824ee071c466ea1b016c94dc7dc225dded
melody_wei <5245624+melody_wei@user.noreply.gitee.com>
2025-06-20 18:02:16 +0800

    1111

--- pages/usercenter/components/user-center-card/index.js
+++ pages/usercenter/components/user-center-card/index.js
@@ -30,5 +30,22 @@ Component({
     gotoUserEditPage() {
       this.triggerEvent('gotoUserEditPage');
     },
+    handleChooseAvatar(e){
+        const { avatarUrl } = e.detail;
+  console.log(avatarUrl);
+  uni.uploadFile({
+    url: getUrlPrefix() + "h5tool/img",
+    filePath: avatarUrl,
+    name: "file",
+    success: (uploadFileRes) => {
+      const avatarUrl = JSON.parse(uploadFileRes.data).data.src;
+      handleSubmit({ avatarUrl });
+    },
+    fail: (error) => {
+      console.log("失败");
+      console.log(error);
+    },
+  });
+    }
   },
 });
